842208518f222f4cbad3a6c19c6a1d9d1ba44c17
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-05-02 01:32:16 -0300

    feat: add some form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.models';
 
 @Component({
@@ -9,8 +10,18 @@ import { Todo } from 'src/models/todo.models';
 export class AppComponent {
   public todos: Todo[] = []
   public title: String = "My Tasks"
+  public form!: FormGroup;
   
-  constructor() {
+  constructor(private fb: FormBuilder) {
+
+    this.form = fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    })
+
     this.todos.push(new Todo(1, "Passear com cachorro", true))
     this.todos.push(new Todo(2, "Ir ao supermercado", true))
     this.todos.push(new Todo(3, "Cortar o cabelo", true))
